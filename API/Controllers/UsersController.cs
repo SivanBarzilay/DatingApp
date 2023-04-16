5f9c413c6b19a0a2b0257bba5b4e5bef0801350c
Sivanba <sivan-ba@hotmail.com>
2023-04-16 12:20:40 +0300

    End  of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+
+
+using System.Security.Cryptography.X509Certificates;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // GET /api/users
+    public class UsersController
+    {
+    
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+           
+        }
+
+        [HttpGet ]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+           
+        }
+
+    }
+}
